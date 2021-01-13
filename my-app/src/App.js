4976b37fc3c894698592329201ce86d8b0963aa5
Lina Ureña <lina8maria8@hotmail.com>
2021-01-13 10:07:44 -0500

    Create React App Workspace

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo React</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Counter = () => {
+    return ( 
+        <h3>My first component</h3>
+     );
+}
+ 
+export default Counter;
